afd1477794a907fa821b8e5fc2d60297b6ab89ee
henrychen0128 <henrychen0128@gmail.com>
2022-04-15 17:16:51 +0800

    test_3:

--- api_post.py
+++ api_post.py
@@ -5,8 +5,7 @@ if __name__ == '__main__':
    my_data = {'store_no':'1',''}
    headers = {'Content-Type': 'application/json'}
    url = ''
-   r = requests.post(url, data=json.dumps(my_data), headers=headers)
-   
+   r = requests.post(url, data=json.dumps(my_data), headers=headers)  
    print(r.status_code)
 
 
